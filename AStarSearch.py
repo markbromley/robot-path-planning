eef4b1d88925ad77f0bf190bd841639d39a920b4
Mark Bromley <mark_bromley@ymail.com>
2016-09-30 14:54:30 +0100

    Added graph representation.

--- AStarSearch.py
+++ AStarSearch.py
@@ -1,3 +1,8 @@
+import PIL
+from PIL import Image
+
+import numpy as np
+
 # Theta* algorithm
 # Jump Point Search
 #http://theory.stanford.edu/~amitp/GameProgramming/AStarComparison.html
@@ -17,11 +22,49 @@ class Graph(object):
     def __init__(self):
         pass
 
+    # :   function Dijkstra(Graph, source):
+    # 2:  for each vertex v in Graph:     // Initialization
+    # 3:  dist[v] := infinity     // initial distance from source to vertex v is set to infinite
+    # 4:  previous[v] := undefined    // Previous node in optimal path from source
+    # 5:  dist[source] := 0   // Distance from source to source
+    # 6:  Q := the set of all nodes in Graph  // all nodes in the graph are unoptimized - thus are in Q
+    # 7:  while Q is not empty:   // main loop
+    # 8:  u := node in Q with smallest dist[ ]
+    # 9:  remove u from Q
+    # 10:     for each neighbor v of u:   // where v has not yet been removed from Q.
+    # 11:     alt := dist[u] + dist_between(u, v)
+    # 12:     if alt < dist[v]    // Relax (u,v)
+    # 13:     dist[v] := alt
+    # 14:     previous[v] := u
+    # 15:     return previous[ ] 
 
 if __name__ == "__main__":
+
+
+    def dijkstra(connections, weights, start, end):
+        inf = float('inf')
+        dist = {vert: inf for vert in enumerate(connections)}
+        previous = {vert: None for vert in enumerate(connections)}
+        dist[start] = 0
+        
+
     aStar = AStarSearch()
-    graph = { 'A' : ['B'],
-              'B' : ['C']}
+
+    connections = { 'A' : ['B', 'C'],
+              'B' : ['A', 'C', 'D'],
+              'C' : ['A', 'B', 'D', 'E'],
+              'D' : ['B', 'C', 'F'],
+              'E' : ['C', 'F'],
+              'F' : ['D', 'E']}
+
+    weights = {'A' : [2, 3],
+               'B' : [2, 3, 7],
+               'C' : [3, 3, 1, 9],
+               'D' : [7, 1, 4],
+               'E' : [9, 1],
+               'F' : [4, 1]}
+
+    print dijkstra(connections, weights, 'A', 'F')
 
     def find_path(graph, start, end, path=[]):
         path = path + [start]
@@ -42,4 +85,14 @@ if __name__ == "__main__":
 
         return None
 
-    print find_path(graph, 'A', 'C')
+
+
+    # graph = connections
+    # print find_path(graph, 'A', 'F')
+
+
+    # im = PIL.Image.open("map.png")
+    # im = im.convert('1')
+    # im = im.resize((200, 200), PIL.Image.ANTIALIAS)
+    # pixels = list(im.getdata())
+    # im.show()
